d34bec57e57e631a093b1fd5167520e2d5942a07
JRizoG <rizo_jorge@outlook.com>
2020-10-29 08:56:35 -0600

    Función rando

--- codigo.R
+++ codigo.R
@@ -15,3 +15,14 @@ print(string4)
 
 string5 <-"Prueba 4"
 print(string5)
+rando <- function(p){
+  #Genera una variable aleatoria en 1,2,...,n dado un vector de distribución
+  u <- rand(1)
+  i <- 1
+  s <- p[1]
+  while ((u>s) && (i<length(p))){
+    i <- i+1
+    s <- s+p[i]
+  }
+  i
+}
