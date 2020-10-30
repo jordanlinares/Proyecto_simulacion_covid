650d47ab24045056a4e1eac4d447bf6f00c0c40e
Lsequeira1 <sequeeduardo@gmail.com>
2020-10-29 20:09:17 -0600

    lineas 38 a 48

--- codigo.R
+++ codigo.R
@@ -15,3 +15,11 @@ print(string4)
 
 string5 <-"Prueba 4"
 print(string5)
+vec1<-c(1,-1,2,0,4,2,2,0,2)
+vec2<-c(1,-1,1,0,1,0,0,1,0)
+vec3<-c(0,1,0,1,2,3,0,-1,0)
+vec4<-c(-1,0,0,0,0,0,1,1,1)
+mat<-cbind(vec2,vec2,vec3,vec4)
+mat
+colnames(mat)<-c("yt","x1t","x2t","x3t")
+View(mat)
