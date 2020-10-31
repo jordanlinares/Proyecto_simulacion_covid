eadc448b0a6e91ba6a97fbd21a0f724db59ca8d6
Jordán Linares <jordanlinares@MacBook-Air-de-Jordan.local>
2020-10-30 19:33:02 -0600

    Código final antes del plotting

--- codigo.R
+++ codigo.R
@@ -1,18 +1,6 @@
 # Esta es una prueba
 library(tidyverse)
 
-vec1<-c(1,-1,2,0,4,2,2,0,2)
-vec2<-c(1,-1,1,0,1,0,0,1,0)
-vec3<-c(0,1,0,1,2,3,0,-1,0)
-vec4<-c(-1,0,0,0,0,0,1,1,1)
-mat<-cbind(vec2,vec2,vec3,vec4)
-mat
-colnames(mat)<-c("yt","x1t","x2t","x3t")
-View(mat)
-runif(1)
-# lÍNEAS 38-48
-Ts<-c(rep(0,n))
-
 rando <- function(p){
   #Genera una variable aleatoria en 1,2,...,n dado un vector de distribución
   u <- rand(1)
@@ -20,18 +8,18 @@ rando <- function(p){
   s <- p[1]
   while ((u>s) && (i<length(p))){
     i <- i+1
-    s <- s+p[i]
+    s <- s + p[i]
   }
   return(i)
 }
 
 #Genera matriz Q
-beta <- 2
+beta  <- 2
 gamma <- 1
-N <-1000
-I0 <-100
-S <-(c(1:(N+2)-1))
-Q <-matrix(rep(0, len = (N+1)*(N+1)), nrow = N+1)
+N     <- 1000
+I0    <- 100
+S     <- c(1:(N+2)-1)
+Q     <- matrix(rep(0, len = (N+1)*(N+1)), nrow = N+1)
 
 lambda <- rep(0,N+1)
 
@@ -42,26 +30,30 @@ for (k in 2:N){
   lambda[k]<-qk
 }
 
-# Aquí deben ir las líneas 24 a 35
+lambda[1]   <- 1 
+lambda[N+1] <- 1
+Q[1,1]      <- 1
+Q[N+1, N+1] <- 1
 
+mu     <- rep(0,N+1)
+mu[I0] <- 1
+Ts     <- NULL
+x      <- NULL
+Ts[1]  <- 0
+x[1]   <- rando(mu)
+i      <- 1
 
-
-
-
-
-
-# lineas 38 48
-while(Ts[i]<20){
-  Ts[i+1]<- Ts[i] -log(runif(1))/lambda[x[i]] #generando una v.a. exponencial para el tiempo de saltos
-  x[i+1]<-rando(Q[x(i),]) # use Q to make state transitions
-  if (x(i)==1){
+# Simulación
+while (T(i) < 20) {
+  Ts[i+1] <- Ts[i] - (log(runif(1))/lambda[x[i]]) #generando una v.a. exponencial para el tiempo de saltos
+  x[i+1]  <- rando(Q[x[i],]) # use Q to make state transitions
+  if (x[i+1] == 1) {
     print("disease died out")
     break
+  } else {
+    i <- i + 1
   }
-  else{
-    i<-i+1
-  }
-  
-  
 }
+
+# Plotting
 
